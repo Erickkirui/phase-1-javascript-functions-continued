03db00658fc619fe6a056763d33e277516e1bb34
Erickkirui <erickkirui653@gmail.com>
2023-03-15 12:35:13 +0300

    created a default funaction and a function level scope

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // code your solution here
+function saturdayFun(activity="roller-skate") {
+
+    return `This Saturday, I want to ${activity}!`
+    
+}
+
+
+const mondayWork = function (activivty ="go to the office"){
+
+    return `This Monday, I will ${activivty}.`
+}
+//console.log(mondayWork("work from home"))
+
+
+function outer (greeting , msg=  "its a fine day to play "){
+
+    return function (name, activity){
+        return `${greeting} ${name} ${msg} ${activity}`
+    }
+
+
+}
+
+//console.log(outer("hello")("erick" , "basketball"));
+
+
+const array = (function(thingToAdd){
+    const base = 3;
+    return [
+      function () {
+         return base + thingToAdd
+
+      },
+      function(){
+
+        return base;
+      },
+
+    ];
+
+})(2);
+
+console.log(array (5))
